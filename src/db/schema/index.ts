4cfe41d617dc46cb18d226e2f177e5a890b4de0a
ahmedgouda5 <ahmedgodayyy@gmail.com>
2026-09-20 21:30:00 +0300

    some update in the course

---
+++ src/db/schema/index.ts
@@ -0,0 +1,6 @@
+export * from "./courses.js";
+export * from "./lessons.js";
+export * from "./options.js";
+export * from "./questions.js";
+export * from "./quizzes.js";
+export * from "./users.js";
--- src/service/courseService.ts
+++ src/service/courseService.ts
@@ -1,46 +1,80 @@
 import { and, eq } from "drizzle-orm";
 import { pool } from "../config/config.js";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { courses } from "../db/schema/courses.js";
+import * as schema from "../db/schema/index.js";
 
-const db = drizzle({ client: pool });
+const db = drizzle({ client: pool, schema });
 
 export class CourseService {
   async addCourse(data: { title: string; price: number; admin_id: string }) {
-    const result = await db.insert(courses).values(data).returning();
+    const result = await db.insert(schema.courses).values(data).returning();
     return result[0];
   }
 
   async getAllCourses() {
-    const result = await db.select().from(courses);
+    const result = await db.query.courses.findMany({
+      with: {
+        lessons: {
+          with: {
+            quiz: {
+              with: {
+                questions: {
+                  with: {
+                    options: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
     return result;
   }
 
   async getCourseById(id: string) {
-    const result = await db
-      .select()
-      .from(courses)
-      .where(eq(courses.id, id));
-    return result[0];
+    const result = await db.query.courses.findFirst({
+      where: eq(schema.courses.id, id),
+      with: {
+        lessons: {
+          with: {
+            quiz: {
+              with: {
+                questions: {
+                  with: {
+                    options: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+    return result;
   }
 
   async updateCourse(
     id: string,
     admin_id: string,
-    data: Partial<{ title: string; price: number }>
+    data: Partial<{ title: string; price: number }>,
   ) {
     const result = await db
-      .update(courses)
+      .update(schema.courses)
       .set(data)
-      .where(and(eq(courses.id, id), eq(courses.admin_id, admin_id)))
+      .where(
+        and(eq(schema.courses.id, id), eq(schema.courses.admin_id, admin_id)),
+      )
       .returning();
     return result[0];
   }
 
   async deleteCourse(id: string, admin_id: string) {
     const result = await db
-      .delete(courses)
-      .where(and(eq(courses.id, id), eq(courses.admin_id, admin_id)))
+      .delete(schema.courses)
+      .where(
+        and(eq(schema.courses.id, id), eq(schema.courses.admin_id, admin_id)),
+      )
       .returning();
     return result[0];
   }
